6ffd8b7b998e07eecaf7163a83860b0e6324c4de
Gökhan ÖZTÜRK <GokhanOzturk@AndroidEdu.IO>
2018-01-27 12:19:46 +0300

    First commit with Love

---
+++ app/src/main/java/androideduio/com/listsample/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package androideduio.com.listsample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
